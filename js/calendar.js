52081f0d17b390e3fa63f2bc8f1d80b85951e88b
nurlailatulhasanah1975-collab <nurlailatul.hasanah1975@gmail.com>
2026-07-17 06:10:16 +0800

    Update calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -46,7 +46,27 @@ getTanggalTerakhir() {
            this.getNamaBulan() + " " +
            this.getTahun();
 
-console.log("calendar.js berhasil dimuat");
+},
+
+getHariTerakhir() {
+
+    const tanggal = new Date(
+        this.getTahun(),
+        this.getNomorBulan() - 1,
+        this.getJumlahHari()
+    );
+
+    const namaHari = [
+        "Minggu",
+        "Senin",
+        "Selasa",
+        "Rabu",
+        "Kamis",
+        "Jumat",
+        "Sabtu"
+    ];
+
+    return namaHari[tanggal.getDay()];
 
 }
 
