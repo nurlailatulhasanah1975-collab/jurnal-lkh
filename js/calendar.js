ffa461506d1018db6afe8af91ba132d551147500
nurlailatulhasanah1975-collab <nurlailatul.hasanah1975@gmail.com>
2026-07-16 23:16:54 +0800

    Add files via upload

---
+++ js/calendar.js
@@ -0,0 +1,53 @@
+/*
+===========================================================
+JURNAL MENGAJAR & LKH
+Mesin Kalender
+MTs. Al Ihsan Tanah Grogot
+
+Version : 1.0
+===========================================================
+*/
+
+const Kalender = {
+
+    getNomorBulan() {
+
+        return Number(document.getElementById("bulanSelect").value);
+
+    },
+
+    getNamaBulan() {
+
+        const bulan = document.getElementById("bulanSelect");
+
+        return bulan.options[bulan.selectedIndex].text;
+
+    },
+
+    getTahun() {
+
+    return Number(document.getElementById("tahunSelect").value);
+
+},
+
+getJumlahHari() {
+
+    const bulan = this.getNomorBulan();
+
+    const tahun = this.getTahun();
+
+    return new Date(tahun, bulan, 0).getDate();
+
+},
+
+getTanggalTerakhir() {
+
+    return this.getJumlahHari() + " " +
+           this.getNamaBulan() + " " +
+           this.getTahun();
+
+console.log("calendar.js berhasil dimuat");
+
+}
+
+};
